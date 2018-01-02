56c2258dfb936656e701fc3f0a3a56f88d36ebe1
jakemakesgames <jakebrown.designer@gmail.com>
2018-01-02 19:14:13 +1100

    changes to second world, added a title menu and script working with keyboard/ gamepad. changes to proj settings - added a new input.

---
+++ Assets/Scripts/TitleSceneChanger.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleSceneChanger : MonoBehaviour
+{
+    private void Update()
+    {
+        if (Input.GetAxis("TitleChange") !=0)
+        {
+            SceneManager.LoadScene("MENU");
+        }
+    }
+}
